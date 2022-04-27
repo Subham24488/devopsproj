cf9343226706cfb27b2f22970005c25e5fb30f68
Subham24488 <54991901+Subham24488@users.noreply.github.com>
2022-04-27 22:55:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Your nodejs project is running,ENJOY!!!!!');
 });
 
 app.listen(PORT, HOST);
